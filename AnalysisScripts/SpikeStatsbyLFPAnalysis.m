b046d992ae8d1f58fad9067330f2d43f8b1ada28
DLevenstein <dan.levenstein@gmail.com>
2018-05-31 20:20:17 -0400

    SpikeStatsbyLFPANalysis:

--- AnalysisScripts/SpikeStatsbyLFPAnalysis.m
+++ AnalysisScripts/SpikeStatsbyLFPAnalysis.m
@@ -1,6 +1,8 @@
 %% DEV
+reporoot = '/home/dlevenstein/ProjectRepos/NeuronalHeterogeneity/'
 basePath = '/Users/dlevenstein/Dropbox/Research/Datasets/20140526_277um';
-figfolder = '/Users/dlevenstein/Project Repos/NeuronalHeterogeneity/AnalysisScripts/AnalysisFigs';
+basePath = pwd;
+figfolder = [reporoot,'AnalysisScripts/AnalysisFigs'];
 baseName = bz_BasenameFromBasepath(basePath);
 
 spikes = bz_GetSpikes('basePath',basePath,'noPrompts',true);
@@ -13,7 +15,7 @@ SleepState = bz_LoadStates(basePath,'SleepState');
 lfp = bz_GetLFP(SleepState.detectorinfo.detectionparms.SleepScoreMetrics.SWchanID,...
     'basepath',basePath,'noPrompts',true);
 
-ISIstats = bz_LoadCellinfo(basePath,'ISIstats');
+ISIStats = bz_LoadCellinfo(basePath,'ISIStats');
 
 %%
 downsamplefactor = 5;
@@ -23,8 +25,49 @@ downsamplefactor = 5;
 %% Interpolate power at each spike
 wavespec.power = log10(abs(wavespec.data));
 
-%% 
-ISIstats.spikepower = cellfun(@(X) interp(wavespec.timestamps,wavespec.power,X,'nearest'),...
-    ISIstats.allspikes.times,'UniformOutput',true)
 
+%%
+statenames = fieldnames(SleepState.ints);
+%% 
+%for ss = 1:length(statenames)
+state = statenames{2};
+    normpower = NormToInt(wavespec.power,'modZ',SleepState.ints.(state),wavespec.samplingRate);
+
+    %%
+    instatespiketimes = cellfun(@(X) InIntervals(X,double(SleepState.ints.(state))),...
+        ISIStats.allspikes.times,'UniformOutput',false);
+    ISIStats.spikepower = cellfun(@(X,Y) interp1(wavespec.timestamps,normpower,...
+        X,'nearest'),...
+        ISIStats.allspikes.times,'UniformOutput',false);
+    
+    %%
+    %<CV2> by power....
+    %CV2-power correlation in each f
+    CV2freqcorr = zeros(spikes.numcells,length(wavespec.freqs));
+    for cc = 1:spikes.numcells
+        cc
+        for ff = 1:length(wavespec.freqs)
+        CV2freqcorr(cc,ff) = corr(ISIStats.spikepower{cc}(instatespiketimes{cc},ff),...
+            ISIStats.allspikes.CV2{cc}(instatespiketimes{cc}),...
+            'type','spearman');
+        end
+    end
+    %%
+    figure
+    imagesc(log10(wavespec.freqs),[1 spikes.numcells],CV2freqcorr)
+    colorbar
+    LogScale('x',10)
+%%
+cellnum = 9;
+freqidx = 30;
+figure
+subplot(2,2,1)
+    plot(ISIStats.spikepower{cellnum}(instatespiketimes{cellnum},freqidx),...
+        ISIStats.allspikes.CV2{cellnum}(instatespiketimes{cellnum}),'.')
+    title(num2str(CV2freqcorr(cellnum,freqidx)))
+%subplot(2,2,2)
+%    plot(ISIStats.allspikes.CV2{cellnum}(instatespiketimes{cellnum})
+
+
+%%
 
