55c4adab1c1d8d69190441a36e3d9fa1db81c801
DLevenstein <dan.levenstein@gmail.com>
2018-06-19 13:20:47 -0400

    added davids dataset

---
+++ AnalysisScripts/HPCSpikeIrregularityAnalysis.m
@@ -0,0 +1,15 @@
+function [ output_args ] = HPCSpikeIrregularityAnalysis( basePath,figfolder )
+%UNTITLED2 Summary of this function goes here
+%   Detailed explanation goes here
+
+basePath = '/mnt/packrat/userdirs/david/zpool2/DT2/DT2_rPPC_rCCG_3540um_1288um_20160227_160227_121226';
+
+%%
+sessionInfo = bz_getSessionInfo(basePath,'editGUI',true);
+
+
+%% Calculate pop CV2 over time
+
+
+end
+
