57b6a73b8d4e338f15734aaff31b082d7f7f1903
dlevenstein <dan.levenstein@gmail.com>
2019-06-17 14:49:27 -0400

    updated SSoptimize to run in parallel oncluster!

--- ClusterCodes/SSOptimizejob.bash
+++ ClusterCodes/SSOptimizejob.bash
@@ -1,9 +1,15 @@
 #!/bin/bash
 #SBATCH -p cpu_short
 #SBATCH --nodes=1
-#SBATCH --tasks-per-node=1
+#SBATCH --tasks-per-node=26
 #SBATCH --time=12:00:00
-#SBATCH --mem-per-cpu=20G
+#SBATCH --mem-per-cpu=32G
 echo $1
 module load matlab/R2018a
+
+export SCRATCH=/gpfs/scratch/dl2820
+mkdir -p $SCRATCH/$SLURM_JOB_ID
+
 matlab -nodisplay -nodesktop -singleCompThread -r "cd('/gpfs/data/buzsakilab/DL/NeuronalHeterogeneity/ClusterCodes');addPaths;bz_RunAnalysis('OptimizeSleepScoreParms','$1','basePath',true);exit;"
+
+rm -rf $SCRATCH/$SLURM_JOB_ID
