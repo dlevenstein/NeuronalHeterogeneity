58363a9f12622a81b1c9053f631349d11c0effd0
DLevenstein <dan.levenstein@gmail.com>
2019-06-17 21:00:56 -0400

    scoring

---
+++ AnalysisScripts/RescoreYS.m
@@ -0,0 +1,39 @@
+function [] = RescoreYS(basePath,figfolder)
+% Date XX/XX/20XX
+%
+%Question: 
+%
+%Plots
+%-
+%-
+%
+%% Set up for parallel in cluster
+% pc = parcluster('local');
+% % store temporary files in the 'scratch' drive on the cluster, labeled by job ID
+% pc.JobStorageLocation = strcat(getenv('SCRATCH'), '/', getenv('SLURM_JOB_ID'));
+% % enable MATLAB to utilize the multiple cores allocated in the job script
+% % SLURM_NTASKS_PER_NODE is a variable set in the job script by the flag --tasks-per-node
+% % we use SLURM_NTASKS_PER_NODE - 1, because one of these tasks is the original MATLAB script itself
+% parpool(pc, str2num(getenv('SLURM_NTASKS_PER_NODE'))-1);
+
+%% Load Header
+%Initiate Paths
+reporoot = '/home/dlevenstein/ProjectRepos/NeuronalHeterogeneity/';
+%reporoot = '/Users/dlevenstein/Project Repos/NeuronalHeterogeneity/';
+%basePath = '/Users/dlevenstein/Dropbox/Research/Datasets/20140526_277um';
+%basePath = '/Users/dlevenstein/Dropbox/Research/Datasets/Cicero_09102014';
+%basePath = '/home/dlevenstein/ProjectRepos/NeuronalHeterogeneity/Datasets/onDesktop/AG_HPC/Achilles_10252013';
+basePath = '/home/dlevenstein/ProjectRepos/NeuronalHeterogeneity/Datasets/onProbox/YS_CTX/YMV08_170922';
+%basePath = pwd;
+figfolder = [reporoot,'AnalysisScripts/AnalysisFigs/DailyAnalysis'];
+baseName = bz_BasenameFromBasepath(basePath);
+
+%%
+load(fullfile(basePath,[baseName,'.SleepScoreLFP.LFP.mat']))
+
+
+%%
+%Keep yuta's time window
+scoretime = [SleepScoreLFP.t(1)-(1./SleepScoreLFP.sf) SleepScoreLFP.t(end)];
+SleepScoreMaster(basePath,'overwrite',true,'scoretime',scoretime)
+
