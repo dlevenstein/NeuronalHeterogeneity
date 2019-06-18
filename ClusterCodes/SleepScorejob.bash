789f14f3e16cb2d9b7cacf67170d905446780e6f
dlevenstein <dan.levenstein@gmail.com>
2019-06-17 21:21:13 -0400

    SSjob submit scrips

--- ClusterCodes/SleepScorejob.bash
+++ ClusterCodes/SleepScorejob.bash
@@ -8,4 +8,4 @@ echo $1
 module load matlab/R2018a
 
 
-matlab -nodisplay -nodesktop -singleCompThread -r "cd('/gpfs/data/buzsakilab/DL/NeuronalHeterogeneity/ClusterCodes');addPaths;bz_RunAnalysis('OptimizeSleepScoreParms','$1','basePath',true);exit;"
+matlab -nodisplay -nodesktop -singleCompThread -r "cd('/gpfs/data/buzsakilab/DL/NeuronalHeterogeneity/ClusterCodes');addPaths;bz_RunAnalysis('RescoreYS','$1','basePath',true);exit;"
