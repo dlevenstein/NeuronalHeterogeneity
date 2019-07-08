138333a64a51cfc19819ae1a16368af5c9a0b516
dlevenstein <dan.levenstein@gmail.com>
2019-07-08 13:07:02 -0400

    increase time for SSOptimize

--- ClusterCodes/SSOptimizejob.bash
+++ ClusterCodes/SSOptimizejob.bash
@@ -1,9 +1,9 @@
 #!/bin/bash
-#SBATCH -p cpu_short
+#SBATCH -p cpu_medium
 #SBATCH --nodes=1
 #SBATCH --tasks-per-node=6
-#SBATCH --time=12:00:00
-#SBATCH --mem-per-cpu=16G
+#SBATCH --time=24:00:00
+#SBATCH --mem-per-cpu=12G
 echo $1
 module load matlab/R2018a
 
