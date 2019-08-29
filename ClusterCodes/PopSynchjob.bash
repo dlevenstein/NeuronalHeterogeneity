189403bc5cec263219e75227b0ed00d8ff5d6346
dlevenstein <dan.levenstein@gmail.com>
2019-08-29 17:01:24 -0400

    move saving to datasets

--- ClusterCodes/PopSynchjob.bash
+++ ClusterCodes/PopSynchjob.bash
@@ -3,10 +3,10 @@
 #SBATCH --nodes=1
 #SBATCH --tasks-per-node=1
 #SBATCH --time=12:00:00
-#SBATCH --mem-per-cpu=36G
+#SBATCH --mem-per-cpu=42G
 echo $1
 module load matlab/R2018a
 
 
-matlab -nodisplay -nodesktop -singleCompThread -r "cd('/gpfs/data/buzsakilab/DL/NeuronalHeterogeneity/ClusterCodes');addPaths;bz_RunAnalysis('SpikeStatsbyPopActivityAnalysis','$1','basePath',true);exit;"
+matlab -nodisplay -nodesktop -singleCompThread -r "cd('/gpfs/data/buzsakilab/DL/NeuronalHeterogeneity/ClusterCodes');addPaths;bz_RunAnalysis('SpikeStatsbyPopActivityAnalysis','$1','basePath',true,'savein','basePath');exit;"
 
