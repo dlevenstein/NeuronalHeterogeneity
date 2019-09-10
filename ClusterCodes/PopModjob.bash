d21ce195d393db3e2a64e16944e003148207cbbe
dlevenstein <dan.levenstein@gmail.com>
2019-09-10 11:50:31 -0400

    Update PopModjob.bash

--- ClusterCodes/PopModjob.bash
+++ ClusterCodes/PopModjob.bash
@@ -1,9 +1,12 @@
 #!/bin/bash
+
+usemem = $2*12
+echo usemem
 #SBATCH -p cpu_medium
 #SBATCH --nodes=1
 #SBATCH --tasks-per-node=1
 #SBATCH --time=2-00:00:00
-#SBATCH --mem-per-cpu=$2*12G
+#SBATCH --mem-per-cpu=$usememG
 echo $1
 module load matlab/R2018a
 
