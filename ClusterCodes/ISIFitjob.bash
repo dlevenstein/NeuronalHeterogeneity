18ddbfb0e307d5339a96ddf0dd69fba22665a178
Dan Levenstein <dan.levenstein@gmail.com>
2020-02-20 16:51:45 -0500

    replace CC

---
+++ ClusterCodes/ISIFitjob.bash
@@ -0,0 +1,11 @@
+#!/bin/bash
+#SBATCH -p cpu_short
+#SBATCH --nodes=1
+#SBATCH --tasks-per-node=1
+#SBATCH --time=12:00:00
+#SBATCH --mem-per-cpu=16G
+echo $1
+module load matlab/R2018a
+
+
+matlab -nodisplay -nodesktop -singleCompThread -r "cd('/gpfs/data/buzsakilab/DL/NeuronalHeterogeneity/ClusterCodes');addPaths;bz_RunAnalysis('GammaModeFitAnalysis','$1','basePath',true,'savein','basePath');exit;"
