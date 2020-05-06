1da598ec8f1f47693bc3d5b3ec6fbb45db93bd37
Dan Levenstein <dan.levenstein@gmail.com>
2020-05-05 23:13:05 -0400

    upload fluctuation sims

---
+++ ClusterCodes/InputFluctSims.bash
@@ -0,0 +1,17 @@
+#!/bin/bash
+#SBATCH -p fn_short
+#SBATCH --nodes=1
+#SBATCH --tasks-per-node=26
+#SBATCH --time=12:00:00
+#SBATCH --mem-per-cpu=12G
+
+savepath=/gpfs/data/buzsakilab/DL/NeuronalHeterogeneity/Modeling/Simulation_Data/iSTDPInputFluctandGSparms
+
+module load matlab/R2018a
+
+export SCRATCH=/gpfs/scratch/dl2820
+mkdir -p $SCRATCH/$SLURM_JOB_ID
+
+matlab -nodisplay -nodesktop -singleCompThread -r "cd('/gpfs/data/buzsakilab/DL/NeuronalHeterogeneity/ClusterCodes');addPaths;tic;iSTDPandGSparms('$savepath');toc;exit;"
+
+rm -rf $SCRATCH/$SLURM_JOB_ID
