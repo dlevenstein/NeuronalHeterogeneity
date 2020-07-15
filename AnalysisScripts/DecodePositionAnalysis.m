5ae85e3a07cd423519b62d4cbe5bd1c109d0682e
Dan Levenstein <dan.levenstein@gmail.com>
2020-07-14 22:14:26 -0400

    oops

--- AnalysisScripts/DecodePositionAnalysis.m
+++ AnalysisScripts/DecodePositionAnalysis.m
@@ -9,15 +9,15 @@ function [ISIbyDecPOS_norm,ISIbyDecPOS_normNREM,CellInfo ] = DecodePositionAnaly
 %
 %% Load Header
 %Initiate Paths
-reporoot = '/Users/dl2820/Project Repos/NeuronalHeterogeneity/';
+%reporoot = '/Users/dl2820/Project Repos/NeuronalHeterogeneity/';
 %reporoot = '/Users/dlevenstein/Project Repos/NeuronalHeterogeneity/';
 %basePath = '/Users/dlevenstein/Dropbox/Research/Datasets/20140526_277um';
 %basePath = [reporoot,'/Datasets/onProbox/AG_HPC/Achilles_10252013'];
 %basePath = '/Users/dl2820/Dropbox/Research/Datasets/Cicero_09102014';
 %basePath = [reporoot,'/Datasets/onProbox/AG_HPC/Achilles_10252013'];
-basePath = '/Users/dl2820/Dropbox/Research/Datasets/Mouse12-120807';
+%basePath = '/Users/dl2820/Dropbox/Research/Datasets/Mouse12-120807';
 %basePath = pwd;
-figfolder = [reporoot,'AnalysisScripts/AnalysisFigs/DailyAnalysis'];
+%figfolder = [reporoot,'AnalysisScripts/AnalysisFigs/DailyAnalysis'];
 baseName = bz_BasenameFromBasepath(basePath);
 
 %Load Stuff
