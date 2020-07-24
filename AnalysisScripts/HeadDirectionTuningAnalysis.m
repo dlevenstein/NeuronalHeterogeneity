f7c87262c53d8acc792fc57e80aa737b6ec2868d
Dan Levenstein <dan.levenstein@gmail.com>
2020-07-24 01:04:08 -0400

    fix place and HD....

--- AnalysisScripts/HeadDirectionTuningAnalysis.m
+++ AnalysisScripts/HeadDirectionTuningAnalysis.m
@@ -9,19 +9,20 @@ function [ISIbyHD_align,MutInfo,ISIbyHD_alignGam ] = HeadDirectionTuningAnalysis
 %
 %% Load Header
 %Initiate Paths
-%reporoot = '/Users/dl2820/Project Repos/NeuronalHeterogeneity/';
+reporoot = '/Users/dl2820/Project Repos/NeuronalHeterogeneity/';
 %reporoot = '/Users/dlevenstein/Project Repos/NeuronalHeterogeneity/';
 %basePath = '/Users/dlevenstein/Dropbox/Research/Datasets/20140526_277um';
 %basePath = [reporoot,'/Datasets/onProbox/AG_HPC/Achilles_10252013'];
 %basePath = '/Users/dl2820/Dropbox/Research/Datasets/Mouse12-120807';
+basePath = '/Users/dl2820/Dropbox/Research/Datasets/Mouse24-131213';
 %basePath = [reporoot,'/Datasets/onProbox/AG_HPC/Achilles_10252013'];
 %basePath = pwd;
-%figfolder = [reporoot,'AnalysisScripts/AnalysisFigs/DailyAnalysis'];
+figfolder = [reporoot,'AnalysisScripts/AnalysisFigs/DailyAnalysis'];
 baseName = bz_BasenameFromBasepath(basePath);
 
 %Load Stuff
 sessionInfo = bz_getSessionInfo(basePath);
-spikes = bz_GetSpikes('basePath',basePath,'noPrompts',true);
+spikes = bz_GetSpikes('basePath',basePath,'noPrompts',true,'region','THAL');
 CellClass = bz_LoadCellinfo(basePath,'CellClass');
 SleepState = bz_LoadStates(basePath,'SleepState');
 %ISIStats = bz_LoadCellinfo(basePath,'ISIStats');
@@ -32,7 +33,6 @@ statecolors = {'k','b','r',[0.6 0.6 0.6]};
 
 %[celltypes,~,typeidx] = unique(CellClass.label);
 %cellcolor = {'k','r'};
-
 
 %%
 headdir.samplingRate = 39.06; %Hz
@@ -56,15 +56,17 @@ numXbins = 100;
 
 %% Get Tuning Specificity (Skaggs)
 
-meanrate = sum(ISIbyHD.Dist.SpikeRate.*ISIbyHD.Dist.pX,2);
+meanrate = nansum(ISIbyHD.Dist.SpikeRate.*ISIbyHD.Dist.pX,2);
 meanrate_full = repmat(meanrate,1,numXbins,1);
-MutInfo.SkaggsInf = squeeze(sum(ISIbyHD.Dist.SpikeRate.*log2(ISIbyHD.Dist.SpikeRate./meanrate_full).*ISIbyHD.Dist.pX,2));
+MutInfo.SkaggsInf = squeeze(nansum(ISIbyHD.Dist.SpikeRate.*log2(ISIbyHD.Dist.SpikeRate./meanrate_full).*ISIbyHD.Dist.pX,2));
 MutInfo.SkaggsInf_sec = MutInfo.SkaggsInf .*squeeze(meanrate);
 
 
 %%
 [~,ISIbyHD.fieldpeak] = max(ISIbyHD.Dist.SpikeRate,[],2);
 ISIbyHD.fieldpeak = ISIbyHD.Dist.Xbins(ISIbyHD.fieldpeak);
+
+%%
 
 %% Compare ISI MI vs Rate MI
 
@@ -119,7 +121,7 @@ for cc = 1:spikes.numcells
 
     cellUID(cc) = spikes.UID(cc);
     GFIDX = find(GammaFit.WAKEstate.cellstats.UID==cellUID(cc));
-    if isempty(GFIDX)
+    if isempty(GFIDX) %Cells with no gamma....
         continue
     end
     cellGamma = GammaFit.WAKEstate.singlecell(GFIDX);
@@ -143,6 +145,9 @@ MIthresh = 0.3;
 % ISIbyHD_alignGam_mean = bz_CollapseStruct( ISIbyHD_alignGam(MutInfo.Rate>MIthresh),3,'mean',true);
 % numcells = sum(MutInfo.Rate>MIthresh);
 % end
+%%
+%MutInfo.cellclass = CellClass;
+%%
 ISIbyHD_align_mean = bz_CollapseStruct( ISIbyHD_align(MutInfo.SkaggsInf>MIthresh),3,'mean',true);
 numcells = sum(MutInfo.SkaggsInf>MIthresh);
 
@@ -151,13 +156,23 @@ ISIbyHD_alignGam_mean = bz_CollapseStruct( ISIbyHD_alignGam(MutInfo.SkaggsInf>MI
 catch
     display('Error Mean')
 end
-
+%%
 ISIbyHD_align = bz_CollapseStruct( ISIbyHD_align,3,'justcat',true);
 ISIbyHD_alignGam = bz_CollapseStruct( ISIbyHD_alignGam,3,'justcat',true);
 
 
+
+
+%% Get Peak Width
+for cc = 1:spikes.numcells
+[~,~,MutInfo.peakwidth(cc)] = findpeaks(ISIbyHD_align.Dist.SpikeRate(1,:,cc),...
+    ISIbyHD_align.Dist.Xbins(1,:,cc),...
+    'NPeaks',1,'SortStr','descend');
+end
+
 %%
-MutInfo.cellclass = CellClass;
+%  figure
+% plot(MutInfo.peakwidth,MutInfo.SkaggsInf,'.')
 %%
 
 % MIforbest = ISIbyHD.MutInf;
@@ -166,45 +181,10 @@ MutInfo.cellclass = CellClass;
 [~,sortMutInfo.ISI] = sort(MutInfo.ISI);
 [~,sortMutInfo.Rate] = sort(MutInfo.Rate);
 [~,sortMutInfo.Skaggs] = sort(MutInfo.SkaggsInf);
+[~,sortMutInfo.peakwidth] = sort(MutInfo.peakwidth);
 
-
-%%
+%% Rate bin
 figure
-subplot(3,3,1)
-
-    imagesc(ISIbyHD_align_mean.Dist.Xbins,[1 spikes.numcells],squeeze(log10(ISIbyHD_align.Dist.SpikeRate(:,:,sortMutInfo.ISI)))')
-    ylabel('Sort by MIISI')
-    hold on
-    plot(ISIbyHD_align_mean.Dist.Xbins([1 end]),spikes.numcells-numcells-0.5.*[1 1],'r')
-subplot(3,3,4)
-    imagesc(ISIbyHD_align_mean.Dist.Xbins,[1 spikes.numcells],squeeze(log10(ISIbyHD_align.Dist.SpikeRate(:,:,sortMutInfo.Rate)))')
-    ylabel('Sort by MIRate')
-    hold on
-    plot(ISIbyHD_align_mean.Dist.Xbins([1 end]),spikes.numcells-numcells-0.5.*[1 1],'r')
-subplot(3,3,7)
-    imagesc(ISIbyHD_alignGam.Dist.Xbins(1,:,1),[1 spikes.numcells],...
-        squeeze((ISIbyHD_alignGam.GammaModes.GSweights(:,:,sortMutInfo.Rate)))')
-    ylabel('Sort by MIRate')
-    hold on
-    plot(ISIbyHD_align_mean.Dist.Xbins([1 end]),spikes.numcells-numcells-0.5.*[1 1],'r')
-    
-    
-subplot(3,3,2)
-    imagesc(ISIbyHD_align_mean.Dist.Xbins,ISIbyHD_align_mean.Dist.Ybins,ISIbyHD_align_mean.Dist.pYX')
-    hold on
-    imagesc(ISIbyHD_align_mean.Dist.Xbins+2.*pi,ISIbyHD_align_mean.Dist.Ybins,ISIbyHD_align_mean.Dist.pYX')
-    plot(ISIbyHD_align_mean.Dist.Xbins,-log10(ISIbyHD_align_mean.Dist.SpikeRate),'r')
-    plot(ISIbyHD_align_mean.Dist.Xbins+2.*pi,-log10(ISIbyHD_align_mean.Dist.SpikeRate),'r')
-    LogScale('y',10,'nohalf',true)
-    ylabel('ISI (s)')
-    bz_AddRightRateAxis
-    xlabel('Position relative to PF Peak (m)')
-    xlim([-pi 3.*pi])
- 
-subplot(3,3,5)    
-    plot(log10(binsizes),MutInfo.Rate_SkaggsCompare)
-    LogScale('x',10)
-    
 subplot(3,3,9)
     plot(log10(MutInfo.Rate),squeeze(log10(ISIbyHD.MutInf)),'.')
     xlabel('MI - rate');ylabel('MI - ISI')
@@ -216,18 +196,74 @@ LogScale('x',10)
 subplot(3,3,3)
 imagesc(log10(binsizes),[1 spikes.numcells],log10(MutInfo.Rate_BinCompare(sortMutInfo.ISI,:)))
 LogScale('x',10)
+
+subplot(3,3,5)    
+    plot(log10(binsizes),MutInfo.Rate_SkaggsCompare)
+    LogScale('x',10)
+    
+subplot(3,3,1)
+
+    imagesc(ISIbyHD_align_mean.Dist.Xbins,[1 spikes.numcells],squeeze(log10(ISIbyHD_align.Dist.SpikeRate(:,:,sortMutInfo.ISI)))')
+    ylabel('Sort by MIISI')
+    hold on
+    plot(ISIbyHD_align_mean.Dist.Xbins([1 end]),spikes.numcells-numcells-0.5.*[1 1],'r')
+subplot(3,3,4)
+    imagesc(ISIbyHD_align_mean.Dist.Xbins,[1 spikes.numcells],squeeze(log10(ISIbyHD_align.Dist.SpikeRate(:,:,sortMutInfo.Rate)))')
+    ylabel('Sort by MIRate')
+    hold on
+    plot(ISIbyHD_align_mean.Dist.Xbins([1 end]),spikes.numcells-numcells-0.5.*[1 1],'r')
+ NiceSave('RateBin',figfolder,baseName)   
+%%
+figure
+
+subplot(3,3,3)
+    imagesc(ISIbyHD_align_mean.Dist.Xbins,[1 spikes.numcells],squeeze(log10(ISIbyHD_align.Dist.SpikeRate(:,:,sortMutInfo.Skaggs)))')
+    ylabel('Sort by MIRate')
+    hold on
+    plot(ISIbyHD_align_mean.Dist.Xbins([1 end]),spikes.numcells-numcells-0.5.*[1 1],'r')
+subplot(3,3,6)
+    imagesc(ISIbyHD_alignGam.Dist.Xbins(1,:,1),[1 spikes.numcells],...
+        squeeze((ISIbyHD_alignGam.GammaModes.GSweights(:,:,sortMutInfo.Skaggs)))')
+    ylabel('Sort by MIRate')
+    hold on
+    plot(ISIbyHD_align_mean.Dist.Xbins([1 end]),spikes.numcells-numcells-0.5.*[1 1],'r')
+    
+    
+subplot(3,3,1)
+    imagesc(ISIbyHD_align_mean.Dist.Xbins,ISIbyHD_align_mean.Dist.Ybins,ISIbyHD_align_mean.Dist.pYX')
+    hold on
+    imagesc(ISIbyHD_align_mean.Dist.Xbins+2.*pi,ISIbyHD_align_mean.Dist.Ybins,ISIbyHD_align_mean.Dist.pYX')
+    plot(ISIbyHD_align_mean.Dist.Xbins,-log10(ISIbyHD_align_mean.Dist.SpikeRate),'r')
+    plot(ISIbyHD_align_mean.Dist.Xbins+2.*pi,-log10(ISIbyHD_align_mean.Dist.SpikeRate),'r')
+    LogScale('y',10,'nohalf',true)
+    ylabel('ISI (s)')
+    bz_AddRightRateAxis
+    xlabel('Position relative to PF Peak (m)')
+    xlim([-pi 3.*pi])
+ 
+try
+subplot(3,3,4)
+plot(ISIbyHD_alignGam_mean.Dist.Xbins,ISIbyHD_alignGam_mean.GammaModes.GSweights,'k')
+hold on
+plot(ISIbyHD_alignGam_mean.Dist.Xbins+2*pi,ISIbyHD_alignGam_mean.GammaModes.GSweights,'k')
+box off 
+axis tight
+catch
+    display('Error Mean')
+end
+
     
 NiceSave('HDCoding',figfolder,baseName)
 
 %%
 figure
 subplot(2,2,2)
-plot((MutInfo.GSrate),(MutInfo.Rate),'.')
+plot((MutInfo.GSrate),(MutInfo.SkaggsInf),'.')
 xlabel('GS Rate');ylabel('MI')
 LogScale('x',10)
 
 subplot(2,2,4)
-plot(MutInfo.GSweight,(MutInfo.Rate),'.')
+plot(MutInfo.GSweight,(MutInfo.SkaggsInf),'.')
 xlabel('GS Weight');ylabel('MI')
 
 subplot(2,2,1)
