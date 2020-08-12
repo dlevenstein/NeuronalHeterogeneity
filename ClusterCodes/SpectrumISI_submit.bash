755fa8ef64ed667eb23671301c1865029f23230f
Dan Levenstein <dan.levenstein@gmail.com>
2020-08-12 19:43:24 -0400

    Update SpectrumISI_submit.bash

--- ClusterCodes/SpectrumISI_submit.bash
+++ ClusterCodes/SpectrumISI_submit.bash
@@ -1,105 +1,5 @@
 #!/bin/bash
-sbatch ./SpectrumISI.bash  /gpfs/data/buzsakilab/DL/Database/YSData/YMV01/YMV01_170818
-sbatch ./SpectrumISI.bash  /gpfs/data/buzsakilab/DL/Database/YSData/YMV02/YMV02_170815
-#sbatch ./SpectrumISI.bash  /gpfs/data/buzsakilab/DL/Database/YSData/YMV03/YMV03_170818
-sbatch ./SpectrumISI.bash  /gpfs/data/buzsakilab/DL/Database/YSData/YMV04/YMV04_170907
-#sbatch ./SpectrumISI.bash  /gpfs/data/buzsakilab/DL/Database/YSData/YMV05/YMV05_170912
-sbatch ./SpectrumISI.bash  /gpfs/data/buzsakilab/DL/Database/YSData/YMV06/YMV06_170913
-#sbatch ./SpectrumISI.bash  /gpfs/data/buzsakilab/DL/Database/YSData/YMV07/YMV07_170914
-#sbatch ./SpectrumISI.bash  /gpfs/data/buzsakilab/DL/Database/YSData/YMV08/YMV08_170922
-sbatch ./SpectrumISI.bash  /gpfs/data/buzsakilab/DL/Database/YSData/YMV09/YMV09_171204
-sbatch ./SpectrumISI.bash  /gpfs/data/buzsakilab/DL/Database/YSData/YMV10/YMV10_171213
-sbatch ./SpectrumISI.bash  /gpfs/data/buzsakilab/DL/Database/YSData/YMV11/YMV11_171208
-sbatch ./SpectrumISI.bash  /gpfs/data/buzsakilab/DL/Database/YSData/YMV12/YMV12_171211
-#sbatch ./SpectrumISI.bash  /gpfs/data/buzsakilab/DL/Database/YSData/YMV13/YMV13_180127
-sbatch ./SpectrumISI.bash  /gpfs/data/buzsakilab/DL/Database/YSData/YMV14/YMV14_180128
-sbatch ./SpectrumISI.bash  /gpfs/data/buzsakilab/DL/Database/YSData/YMV15/YMV15_180205
-sbatch ./SpectrumISI.bash  /gpfs/data/buzsakilab/DL/Database/YSData/YMV16/YMV16_180206
-sbatch ./SpectrumISI.bash  /gpfs/data/buzsakilab/DL/Database/YSData/YMV17/YMV17_180207
-sbatch ./SpectrumISI.bash  /gpfs/data/buzsakilab/DL/Database/YSData/YMV18/YMV18_180208
-sbatch ./SpectrumISI.bash  /gpfs/data/buzsakilab/DL/Database/YSData/YMV19/YMV19_180209
-#
-sbatch ./SpectrumISI.bash  /gpfs/data/buzsakilab/DL/Database/BWData/Bogey/Bogey_012615
-sbatch ./SpectrumISI.bash  /gpfs/data/buzsakilab/DL/Database/BWData/BWRat17/BWRat17_121712
-sbatch ./SpectrumISI.bash  /gpfs/data/buzsakilab/DL/Database/BWData/BWRat17/BWRat17_121912
-sbatch ./SpectrumISI.bash  /gpfs/data/buzsakilab/DL/Database/BWData/BWRat18/BWRat18_020513
-sbatch ./SpectrumISI.bash  /gpfs/data/buzsakilab/DL/Database/BWData/BWRat19/BWRat19_032413
-sbatch ./SpectrumISI.bash  /gpfs/data/buzsakilab/DL/Database/BWData/BWRat19/BWRat19_032513
-sbatch ./SpectrumISI.bash  /gpfs/data/buzsakilab/DL/Database/BWData/BWRat20/BWRat20_101013
-sbatch ./SpectrumISI.bash  /gpfs/data/buzsakilab/DL/Database/BWData/BWRat20/BWRat20_101513
-sbatch ./SpectrumISI.bash  /gpfs/data/buzsakilab/DL/Database/BWData/BWRat21/BWRat21_121113
-sbatch ./SpectrumISI.bash  /gpfs/data/buzsakilab/DL/Database/BWData/BWRat21/BWRat21_121613
-sbatch ./SpectrumISI.bash  /gpfs/data/buzsakilab/DL/Database/BWData/BWRat21/BWRat21_121813
-sbatch ./SpectrumISI.bash  /gpfs/data/buzsakilab/DL/Database/BWData/Dino/Dino_061814
-sbatch ./SpectrumISI.bash  /gpfs/data/buzsakilab/DL/Database/BWData/Dino/Dino_061914
-sbatch ./SpectrumISI.bash  /gpfs/data/buzsakilab/DL/Database/BWData/Dino/Dino_062014
-sbatch ./SpectrumISI.bash  /gpfs/data/buzsakilab/DL/Database/BWData/Dino/Dino_072114
-sbatch ./SpectrumISI.bash  /gpfs/data/buzsakilab/DL/Database/BWData/Dino/Dino_072314
-sbatch ./SpectrumISI.bash  /gpfs/data/buzsakilab/DL/Database/BWData/Dino/Dino_072414
-sbatch ./SpectrumISI.bash  /gpfs/data/buzsakilab/DL/Database/BWData/JennBuzsaki22/20140526_277um
-sbatch ./SpectrumISI.bash  /gpfs/data/buzsakilab/DL/Database/BWData/JennBuzsaki22/20140527_421um
-sbatch ./SpectrumISI.bash  /gpfs/data/buzsakilab/DL/Database/BWData/JennBuzsaki22/20140528_565um
-sbatch ./SpectrumISI.bash  /gpfs/data/buzsakilab/DL/Database/BWData/Rizzo/Rizzo_022615
-sbatch ./SpectrumISI.bash  /gpfs/data/buzsakilab/DL/Database/BWData/Rizzo/Rizzo_022715
-sbatch ./SpectrumISI.bash  /gpfs/data/buzsakilab/DL/Database/BWData/Splinter/Splinter_020515
-sbatch ./SpectrumISI.bash  /gpfs/data/buzsakilab/DL/Database/BWData/Splinter/Splinter_020915
-sbatch ./SpectrumISI.bash  /gpfs/data/buzsakilab/DL/Database/BWData/Templeton/Templeton_032415
 
-sbatch ./SpectrumISI.bash  /gpfs/data/buzsakilab/DL/Database/AGData/Achilles/Achilles_11012013
-sbatch ./SpectrumISI.bash  /gpfs/data/buzsakilab/DL/Database/AGData/Achilles/Achilles_10252013
-sbatch ./SpectrumISI.bash  /gpfs/data/buzsakilab/DL/Database/AGData/Buddy/Buddy_06272013
-sbatch ./SpectrumISI.bash  /gpfs/data/buzsakilab/DL/Database/AGData/Cicero/Cicero_09102014
-sbatch ./SpectrumISI.bash  /gpfs/data/buzsakilab/DL/Database/AGData/Cicero/Cicero_09012014
-sbatch ./SpectrumISI.bash  /gpfs/data/buzsakilab/DL/Database/AGData/Cicero/Cicero_09172014
-sbatch ./SpectrumISI.bash  /gpfs/data/buzsakilab/DL/Database/AGData/Gatsby/Gatsby_08282013
-sbatch ./SpectrumISI.bash  /gpfs/data/buzsakilab/DL/Database/AGData/Gatsby/Gatsby_08022013
-#
-#
-#
-sbatch ./SpectrumISI.bash  /gpfs/data/buzsakilab/DL/Database/APData/Mouse12/Mouse12-120806
-sbatch ./SpectrumISI.bash  /gpfs/data/buzsakilab/DL/Database/APData/Mouse12/Mouse12-120807
-sbatch ./SpectrumISI.bash  /gpfs/data/buzsakilab/DL/Database/APData/Mouse12/Mouse12-120808
-sbatch ./SpectrumISI.bash  /gpfs/data/buzsakilab/DL/Database/APData/Mouse12/Mouse12-120809
-sbatch ./SpectrumISI.bash  /gpfs/data/buzsakilab/DL/Database/APData/Mouse12/Mouse12-120810
-sbatch ./SpectrumISI.bash  /gpfs/data/buzsakilab/DL/Database/APData/Mouse17/Mouse17-130125
-sbatch ./SpectrumISI.bash  /gpfs/data/buzsakilab/DL/Database/APData/Mouse17/Mouse17-130128
-sbatch ./SpectrumISI.bash  /gpfs/data/buzsakilab/DL/Database/APData/Mouse17/Mouse17-130129
-sbatch ./SpectrumISI.bash  /gpfs/data/buzsakilab/DL/Database/APData/Mouse17/Mouse17-130130
-sbatch ./SpectrumISI.bash  /gpfs/data/buzsakilab/DL/Database/APData/Mouse17/Mouse17-130131
-sbatch ./SpectrumISI.bash  /gpfs/data/buzsakilab/DL/Database/APData/Mouse17/Mouse17-130201
-sbatch ./SpectrumISI.bash  /gpfs/data/buzsakilab/DL/Database/APData/Mouse17/Mouse17-130202
-sbatch ./SpectrumISI.bash  /gpfs/data/buzsakilab/DL/Database/APData/Mouse17/Mouse17-130203
-sbatch ./SpectrumISI.bash  /gpfs/data/buzsakilab/DL/Database/APData/Mouse17/Mouse17-130204
-sbatch ./SpectrumISI.bash  /gpfs/data/buzsakilab/DL/Database/APData/Mouse20/Mouse20-130514
-sbatch ./SpectrumISI.bash  /gpfs/data/buzsakilab/DL/Database/APData/Mouse20/Mouse20-130515
-sbatch ./SpectrumISI.bash  /gpfs/data/buzsakilab/DL/Database/APData/Mouse20/Mouse20-130516
-sbatch ./SpectrumISI.bash  /gpfs/data/buzsakilab/DL/Database/APData/Mouse20/Mouse20-130517
-sbatch ./SpectrumISI.bash  /gpfs/data/buzsakilab/DL/Database/APData/Mouse20/Mouse20-130520
-sbatch ./SpectrumISI.bash  /gpfs/data/buzsakilab/DL/Database/APData/Mouse24/Mouse24-131213
-sbatch ./SpectrumISI.bash  /gpfs/data/buzsakilab/DL/Database/APData/Mouse24/Mouse24-131216
-sbatch ./SpectrumISI.bash  /gpfs/data/buzsakilab/DL/Database/APData/Mouse24/Mouse24-131217
-sbatch ./SpectrumISI.bash  /gpfs/data/buzsakilab/DL/Database/APData/Mouse24/Mouse24-131218
-sbatch ./SpectrumISI.bash  /gpfs/data/buzsakilab/DL/Database/APData/Mouse25/Mouse25-140123
-sbatch ./SpectrumISI.bash  /gpfs/data/buzsakilab/DL/Database/APData/Mouse25/Mouse25-140124
-sbatch ./SpectrumISI.bash  /gpfs/data/buzsakilab/DL/Database/APData/Mouse25/Mouse25-140128
-sbatch ./SpectrumISI.bash  /gpfs/data/buzsakilab/DL/Database/APData/Mouse25/Mouse25-140129
-sbatch ./SpectrumISI.bash  /gpfs/data/buzsakilab/DL/Database/APData/Mouse25/Mouse25-140130
-sbatch ./SpectrumISI.bash  /gpfs/data/buzsakilab/DL/Database/APData/Mouse25/Mouse25-140131
-sbatch ./SpectrumISI.bash  /gpfs/data/buzsakilab/DL/Database/APData/Mouse25/Mouse25-140203
-sbatch ./SpectrumISI.bash  /gpfs/data/buzsakilab/DL/Database/APData/Mouse25/Mouse25-140204
-sbatch ./SpectrumISI.bash  /gpfs/data/buzsakilab/DL/Database/APData/Mouse25/Mouse25-140205
-sbatch ./SpectrumISI.bash  /gpfs/data/buzsakilab/DL/Database/APData/Mouse25/Mouse25-140206
-sbatch ./SpectrumISI.bash  /gpfs/data/buzsakilab/DL/Database/APData/Mouse28/Mouse28-140310
-sbatch ./SpectrumISI.bash  /gpfs/data/buzsakilab/DL/Database/APData/Mouse28/Mouse28-140311
-sbatch ./SpectrumISI.bash  /gpfs/data/buzsakilab/DL/Database/APData/Mouse28/Mouse28-140312
-sbatch ./SpectrumISI.bash  /gpfs/data/buzsakilab/DL/Database/APData/Mouse28/Mouse28-140313
-sbatch ./SpectrumISI.bash  /gpfs/data/buzsakilab/DL/Database/APData/Mouse28/Mouse28-140314
-sbatch ./SpectrumISI.bash  /gpfs/data/buzsakilab/DL/Database/APData/Mouse28/Mouse28-140317
-sbatch ./SpectrumISI.bash  /gpfs/data/buzsakilab/DL/Database/APData/Mouse28/Mouse28-140318
-sbatch ./SpectrumISI.bash  /gpfs/data/buzsakilab/DL/Database/APData/Mouse32/Mouse32-140822
-#
-#
-#
 sbatch ./SpectrumISI_bla.bash  /gpfs/data/buzsakilab/DL/Database/GGData/Rat08/Rat08-20130708/ .
 sbatch ./SpectrumISI_bla.bash  /gpfs/data/buzsakilab/DL/Database/GGData/Rat08/Rat08-20130709/ .
 sbatch ./SpectrumISI_bla.bash  /gpfs/data/buzsakilab/DL/Database/GGData/Rat08/Rat08-20130710/ .
@@ -210,3 +110,107 @@ sbatch ./SpectrumISI_pir.bash  /gpfs/data/buzsakilab/DL/Database/GGData/Rat11/Ra
 #sbatch ./SpectrumISI_pir.bash  /gpfs/data/buzsakilab/DL/Database/GGData/Rat11/Rat11-20150401/ .
 #sbatch ./SpectrumISI_pir.bash  /gpfs/data/buzsakilab/DL/Database/GGData/Rat11/Rat11-20150402/ .
 #sbatch ./SpectrumISI_pir.bash  /gpfs/data/buzsakilab/DL/Database/GGData/Rat11/Rat11-20150403/ .
+
+
+
+sbatch ./SpectrumISI.bash  /gpfs/data/buzsakilab/DL/Database/YSData/YMV01/YMV01_170818
+sbatch ./SpectrumISI.bash  /gpfs/data/buzsakilab/DL/Database/YSData/YMV02/YMV02_170815
+#sbatch ./SpectrumISI.bash  /gpfs/data/buzsakilab/DL/Database/YSData/YMV03/YMV03_170818
+sbatch ./SpectrumISI.bash  /gpfs/data/buzsakilab/DL/Database/YSData/YMV04/YMV04_170907
+#sbatch ./SpectrumISI.bash  /gpfs/data/buzsakilab/DL/Database/YSData/YMV05/YMV05_170912
+sbatch ./SpectrumISI.bash  /gpfs/data/buzsakilab/DL/Database/YSData/YMV06/YMV06_170913
+#sbatch ./SpectrumISI.bash  /gpfs/data/buzsakilab/DL/Database/YSData/YMV07/YMV07_170914
+#sbatch ./SpectrumISI.bash  /gpfs/data/buzsakilab/DL/Database/YSData/YMV08/YMV08_170922
+sbatch ./SpectrumISI.bash  /gpfs/data/buzsakilab/DL/Database/YSData/YMV09/YMV09_171204
+sbatch ./SpectrumISI.bash  /gpfs/data/buzsakilab/DL/Database/YSData/YMV10/YMV10_171213
+sbatch ./SpectrumISI.bash  /gpfs/data/buzsakilab/DL/Database/YSData/YMV11/YMV11_171208
+sbatch ./SpectrumISI.bash  /gpfs/data/buzsakilab/DL/Database/YSData/YMV12/YMV12_171211
+#sbatch ./SpectrumISI.bash  /gpfs/data/buzsakilab/DL/Database/YSData/YMV13/YMV13_180127
+sbatch ./SpectrumISI.bash  /gpfs/data/buzsakilab/DL/Database/YSData/YMV14/YMV14_180128
+sbatch ./SpectrumISI.bash  /gpfs/data/buzsakilab/DL/Database/YSData/YMV15/YMV15_180205
+sbatch ./SpectrumISI.bash  /gpfs/data/buzsakilab/DL/Database/YSData/YMV16/YMV16_180206
+sbatch ./SpectrumISI.bash  /gpfs/data/buzsakilab/DL/Database/YSData/YMV17/YMV17_180207
+sbatch ./SpectrumISI.bash  /gpfs/data/buzsakilab/DL/Database/YSData/YMV18/YMV18_180208
+sbatch ./SpectrumISI.bash  /gpfs/data/buzsakilab/DL/Database/YSData/YMV19/YMV19_180209
+#
+sbatch ./SpectrumISI.bash  /gpfs/data/buzsakilab/DL/Database/BWData/Bogey/Bogey_012615
+sbatch ./SpectrumISI.bash  /gpfs/data/buzsakilab/DL/Database/BWData/BWRat17/BWRat17_121712
+sbatch ./SpectrumISI.bash  /gpfs/data/buzsakilab/DL/Database/BWData/BWRat17/BWRat17_121912
+sbatch ./SpectrumISI.bash  /gpfs/data/buzsakilab/DL/Database/BWData/BWRat18/BWRat18_020513
+sbatch ./SpectrumISI.bash  /gpfs/data/buzsakilab/DL/Database/BWData/BWRat19/BWRat19_032413
+sbatch ./SpectrumISI.bash  /gpfs/data/buzsakilab/DL/Database/BWData/BWRat19/BWRat19_032513
+sbatch ./SpectrumISI.bash  /gpfs/data/buzsakilab/DL/Database/BWData/BWRat20/BWRat20_101013
+sbatch ./SpectrumISI.bash  /gpfs/data/buzsakilab/DL/Database/BWData/BWRat20/BWRat20_101513
+sbatch ./SpectrumISI.bash  /gpfs/data/buzsakilab/DL/Database/BWData/BWRat21/BWRat21_121113
+sbatch ./SpectrumISI.bash  /gpfs/data/buzsakilab/DL/Database/BWData/BWRat21/BWRat21_121613
+sbatch ./SpectrumISI.bash  /gpfs/data/buzsakilab/DL/Database/BWData/BWRat21/BWRat21_121813
+sbatch ./SpectrumISI.bash  /gpfs/data/buzsakilab/DL/Database/BWData/Dino/Dino_061814
+sbatch ./SpectrumISI.bash  /gpfs/data/buzsakilab/DL/Database/BWData/Dino/Dino_061914
+sbatch ./SpectrumISI.bash  /gpfs/data/buzsakilab/DL/Database/BWData/Dino/Dino_062014
+sbatch ./SpectrumISI.bash  /gpfs/data/buzsakilab/DL/Database/BWData/Dino/Dino_072114
+sbatch ./SpectrumISI.bash  /gpfs/data/buzsakilab/DL/Database/BWData/Dino/Dino_072314
+sbatch ./SpectrumISI.bash  /gpfs/data/buzsakilab/DL/Database/BWData/Dino/Dino_072414
+sbatch ./SpectrumISI.bash  /gpfs/data/buzsakilab/DL/Database/BWData/JennBuzsaki22/20140526_277um
+sbatch ./SpectrumISI.bash  /gpfs/data/buzsakilab/DL/Database/BWData/JennBuzsaki22/20140527_421um
+sbatch ./SpectrumISI.bash  /gpfs/data/buzsakilab/DL/Database/BWData/JennBuzsaki22/20140528_565um
+sbatch ./SpectrumISI.bash  /gpfs/data/buzsakilab/DL/Database/BWData/Rizzo/Rizzo_022615
+sbatch ./SpectrumISI.bash  /gpfs/data/buzsakilab/DL/Database/BWData/Rizzo/Rizzo_022715
+sbatch ./SpectrumISI.bash  /gpfs/data/buzsakilab/DL/Database/BWData/Splinter/Splinter_020515
+sbatch ./SpectrumISI.bash  /gpfs/data/buzsakilab/DL/Database/BWData/Splinter/Splinter_020915
+sbatch ./SpectrumISI.bash  /gpfs/data/buzsakilab/DL/Database/BWData/Templeton/Templeton_032415
+
+sbatch ./SpectrumISI.bash  /gpfs/data/buzsakilab/DL/Database/AGData/Achilles/Achilles_11012013
+sbatch ./SpectrumISI.bash  /gpfs/data/buzsakilab/DL/Database/AGData/Achilles/Achilles_10252013
+sbatch ./SpectrumISI.bash  /gpfs/data/buzsakilab/DL/Database/AGData/Buddy/Buddy_06272013
+sbatch ./SpectrumISI.bash  /gpfs/data/buzsakilab/DL/Database/AGData/Cicero/Cicero_09102014
+sbatch ./SpectrumISI.bash  /gpfs/data/buzsakilab/DL/Database/AGData/Cicero/Cicero_09012014
+sbatch ./SpectrumISI.bash  /gpfs/data/buzsakilab/DL/Database/AGData/Cicero/Cicero_09172014
+sbatch ./SpectrumISI.bash  /gpfs/data/buzsakilab/DL/Database/AGData/Gatsby/Gatsby_08282013
+sbatch ./SpectrumISI.bash  /gpfs/data/buzsakilab/DL/Database/AGData/Gatsby/Gatsby_08022013
+#
+#
+#
+sbatch ./SpectrumISI.bash  /gpfs/data/buzsakilab/DL/Database/APData/Mouse12/Mouse12-120806
+sbatch ./SpectrumISI.bash  /gpfs/data/buzsakilab/DL/Database/APData/Mouse12/Mouse12-120807
+sbatch ./SpectrumISI.bash  /gpfs/data/buzsakilab/DL/Database/APData/Mouse12/Mouse12-120808
+sbatch ./SpectrumISI.bash  /gpfs/data/buzsakilab/DL/Database/APData/Mouse12/Mouse12-120809
+sbatch ./SpectrumISI.bash  /gpfs/data/buzsakilab/DL/Database/APData/Mouse12/Mouse12-120810
+sbatch ./SpectrumISI.bash  /gpfs/data/buzsakilab/DL/Database/APData/Mouse17/Mouse17-130125
+sbatch ./SpectrumISI.bash  /gpfs/data/buzsakilab/DL/Database/APData/Mouse17/Mouse17-130128
+sbatch ./SpectrumISI.bash  /gpfs/data/buzsakilab/DL/Database/APData/Mouse17/Mouse17-130129
+sbatch ./SpectrumISI.bash  /gpfs/data/buzsakilab/DL/Database/APData/Mouse17/Mouse17-130130
+sbatch ./SpectrumISI.bash  /gpfs/data/buzsakilab/DL/Database/APData/Mouse17/Mouse17-130131
+sbatch ./SpectrumISI.bash  /gpfs/data/buzsakilab/DL/Database/APData/Mouse17/Mouse17-130201
+sbatch ./SpectrumISI.bash  /gpfs/data/buzsakilab/DL/Database/APData/Mouse17/Mouse17-130202
+sbatch ./SpectrumISI.bash  /gpfs/data/buzsakilab/DL/Database/APData/Mouse17/Mouse17-130203
+sbatch ./SpectrumISI.bash  /gpfs/data/buzsakilab/DL/Database/APData/Mouse17/Mouse17-130204
+sbatch ./SpectrumISI.bash  /gpfs/data/buzsakilab/DL/Database/APData/Mouse20/Mouse20-130514
+sbatch ./SpectrumISI.bash  /gpfs/data/buzsakilab/DL/Database/APData/Mouse20/Mouse20-130515
+sbatch ./SpectrumISI.bash  /gpfs/data/buzsakilab/DL/Database/APData/Mouse20/Mouse20-130516
+sbatch ./SpectrumISI.bash  /gpfs/data/buzsakilab/DL/Database/APData/Mouse20/Mouse20-130517
+sbatch ./SpectrumISI.bash  /gpfs/data/buzsakilab/DL/Database/APData/Mouse20/Mouse20-130520
+sbatch ./SpectrumISI.bash  /gpfs/data/buzsakilab/DL/Database/APData/Mouse24/Mouse24-131213
+sbatch ./SpectrumISI.bash  /gpfs/data/buzsakilab/DL/Database/APData/Mouse24/Mouse24-131216
+sbatch ./SpectrumISI.bash  /gpfs/data/buzsakilab/DL/Database/APData/Mouse24/Mouse24-131217
+sbatch ./SpectrumISI.bash  /gpfs/data/buzsakilab/DL/Database/APData/Mouse24/Mouse24-131218
+sbatch ./SpectrumISI.bash  /gpfs/data/buzsakilab/DL/Database/APData/Mouse25/Mouse25-140123
+sbatch ./SpectrumISI.bash  /gpfs/data/buzsakilab/DL/Database/APData/Mouse25/Mouse25-140124
+sbatch ./SpectrumISI.bash  /gpfs/data/buzsakilab/DL/Database/APData/Mouse25/Mouse25-140128
+sbatch ./SpectrumISI.bash  /gpfs/data/buzsakilab/DL/Database/APData/Mouse25/Mouse25-140129
+sbatch ./SpectrumISI.bash  /gpfs/data/buzsakilab/DL/Database/APData/Mouse25/Mouse25-140130
+sbatch ./SpectrumISI.bash  /gpfs/data/buzsakilab/DL/Database/APData/Mouse25/Mouse25-140131
+sbatch ./SpectrumISI.bash  /gpfs/data/buzsakilab/DL/Database/APData/Mouse25/Mouse25-140203
+sbatch ./SpectrumISI.bash  /gpfs/data/buzsakilab/DL/Database/APData/Mouse25/Mouse25-140204
+sbatch ./SpectrumISI.bash  /gpfs/data/buzsakilab/DL/Database/APData/Mouse25/Mouse25-140205
+sbatch ./SpectrumISI.bash  /gpfs/data/buzsakilab/DL/Database/APData/Mouse25/Mouse25-140206
+sbatch ./SpectrumISI.bash  /gpfs/data/buzsakilab/DL/Database/APData/Mouse28/Mouse28-140310
+sbatch ./SpectrumISI.bash  /gpfs/data/buzsakilab/DL/Database/APData/Mouse28/Mouse28-140311
+sbatch ./SpectrumISI.bash  /gpfs/data/buzsakilab/DL/Database/APData/Mouse28/Mouse28-140312
+sbatch ./SpectrumISI.bash  /gpfs/data/buzsakilab/DL/Database/APData/Mouse28/Mouse28-140313
+sbatch ./SpectrumISI.bash  /gpfs/data/buzsakilab/DL/Database/APData/Mouse28/Mouse28-140314
+sbatch ./SpectrumISI.bash  /gpfs/data/buzsakilab/DL/Database/APData/Mouse28/Mouse28-140317
+sbatch ./SpectrumISI.bash  /gpfs/data/buzsakilab/DL/Database/APData/Mouse28/Mouse28-140318
+sbatch ./SpectrumISI.bash  /gpfs/data/buzsakilab/DL/Database/APData/Mouse32/Mouse32-140822
+#
+#
+#
