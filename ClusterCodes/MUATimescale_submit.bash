7c5b3ed3f3cd49976d91c77e60acc91692796621
Dan Levenstein <dan.levenstein@gmail.com>
2020-08-26 16:10:27 -0400

    move timescale

---
+++ ClusterCodes/MUATimescale_submit.bash
@@ -0,0 +1,216 @@
+#!/bin/bash
+
+sbatch ./MUATimescale_bla.bash  /gpfs/data/buzsakilab/DL/Database/GGData/Rat08/Rat08-20130708/ .
+sbatch ./MUATimescale_bla.bash  /gpfs/data/buzsakilab/DL/Database/GGData/Rat08/Rat08-20130709/ .
+sbatch ./MUATimescale_bla.bash  /gpfs/data/buzsakilab/DL/Database/GGData/Rat08/Rat08-20130710/ .
+sbatch ./MUATimescale_bla.bash  /gpfs/data/buzsakilab/DL/Database/GGData/Rat08/Rat08-20130711/ .
+sbatch ./MUATimescale_bla.bash  /gpfs/data/buzsakilab/DL/Database/GGData/Rat08/Rat08-20130712/ .
+sbatch ./MUATimescale_bla.bash  /gpfs/data/buzsakilab/DL/Database/GGData/Rat08/Rat08-20130713/ .
+sbatch ./MUATimescale_bla.bash  /gpfs/data/buzsakilab/DL/Database/GGData/Rat08/Rat08-20130715/ .
+sbatch ./MUATimescale_bla.bash  /gpfs/data/buzsakilab/DL/Database/GGData/Rat08/Rat08-20130716/ .
+sbatch ./MUATimescale_bla.bash  /gpfs/data/buzsakilab/DL/Database/GGData/Rat08/Rat08-20130717/ .
+#sbatch ./MUATimescale_bla.bash  /gpfs/data/buzsakilab/DL/Database/GGData/Rat08/Rat08-20130718/ .
+sbatch ./MUATimescale_bla.bash  /gpfs/data/buzsakilab/DL/Database/GGData/Rat08/Rat08-20130719/ .
+#sbatch ./MUATimescale_bla.bash  /gpfs/data/buzsakilab/DL/Database/GGData/Rat08/Rat08-20130720/ .
+#sbatch ./MUATimescale_bla.bash  /gpfs/data/buzsakilab/DL/Database/GGData/Rat08/Rat08-20130722/ .
+sbatch ./MUATimescale_bla.bash  /gpfs/data/buzsakilab/DL/Database/GGData/Rat09/Rat09-20140324/ .
+sbatch ./MUATimescale_bla.bash  /gpfs/data/buzsakilab/DL/Database/GGData/Rat09/Rat09-20140325/ .
+sbatch ./MUATimescale_bla.bash  /gpfs/data/buzsakilab/DL/Database/GGData/Rat09/Rat09-20140326/ .
+sbatch ./MUATimescale_bla.bash  /gpfs/data/buzsakilab/DL/Database/GGData/Rat09/Rat09-20140327/ .
+sbatch ./MUATimescale_bla.bash  /gpfs/data/buzsakilab/DL/Database/GGData/Rat09/Rat09-20140328/ .
+sbatch ./MUATimescale_bla.bash  /gpfs/data/buzsakilab/DL/Database/GGData/Rat09/Rat09-20140329/ .
+#sbatch ./MUATimescale_bla.bash  /gpfs/data/buzsakilab/DL/Database/GGData/Rat09/Rat09-20140331/ .
+#sbatch ./MUATimescale_bla.bash  /gpfs/data/buzsakilab/DL/Database/GGData/Rat09/Rat09-20140401/ .
+#sbatch ./MUATimescale_bla.bash  /gpfs/data/buzsakilab/DL/Database/GGData/Rat09/Rat09-20140402/ .
+#sbatch ./MUATimescale_bla.bash  /gpfs/data/buzsakilab/DL/Database/GGData/Rat09/Rat09-20140403/ .
+#sbatch ./MUATimescale_bla.bash  /gpfs/data/buzsakilab/DL/Database/GGData/Rat09/Rat09-20140404/ .
+#sbatch ./MUATimescale_bla.bash  /gpfs/data/buzsakilab/DL/Database/GGData/Rat09/Rat09-20140405/ .
+#sbatch ./MUATimescale_bla.bash  /gpfs/data/buzsakilab/DL/Database/GGData/Rat09/Rat09-20140407/ .
+#sbatch ./MUATimescale_bla.bash  /gpfs/data/buzsakilab/DL/Database/GGData/Rat09/Rat09-20140408/ .
+#sbatch ./MUATimescale_bla.bash  /gpfs/data/buzsakilab/DL/Database/GGData/Rat09/Rat09-20140409/ .
+sbatch ./MUATimescale_bla.bash  /gpfs/data/buzsakilab/DL/Database/GGData/Rat10/Rat10-20140619/ .
+sbatch ./MUATimescale_bla.bash  /gpfs/data/buzsakilab/DL/Database/GGData/Rat10/Rat10-20140620/ .
+sbatch ./MUATimescale_bla.bash  /gpfs/data/buzsakilab/DL/Database/GGData/Rat10/Rat10-20140622/ .
+sbatch ./MUATimescale_bla.bash  /gpfs/data/buzsakilab/DL/Database/GGData/Rat10/Rat10-20140624/ .
+sbatch ./MUATimescale_bla.bash  /gpfs/data/buzsakilab/DL/Database/GGData/Rat10/Rat10-20140626/ .
+sbatch ./MUATimescale_bla.bash  /gpfs/data/buzsakilab/DL/Database/GGData/Rat10/Rat10-20140627/ .
+sbatch ./MUATimescale_bla.bash  /gpfs/data/buzsakilab/DL/Database/GGData/Rat10/Rat10-20140628/ .
+sbatch ./MUATimescale_bla.bash  /gpfs/data/buzsakilab/DL/Database/GGData/Rat10/Rat10-20140629/ .
+sbatch ./MUATimescale_bla.bash  /gpfs/data/buzsakilab/DL/Database/GGData/Rat10/Rat10-20140701/ .
+sbatch ./MUATimescale_bla.bash  /gpfs/data/buzsakilab/DL/Database/GGData/Rat10/Rat10-20140702/ .
+sbatch ./MUATimescale_bla.bash  /gpfs/data/buzsakilab/DL/Database/GGData/Rat10/Rat10-20140703/ .
+sbatch ./MUATimescale_bla.bash  /gpfs/data/buzsakilab/DL/Database/GGData/Rat10/Rat10-20140704/ .
+#sbatch ./MUATimescale_bla.bash  /gpfs/data/buzsakilab/DL/Database/GGData/Rat10/Rat10-20140705/ .  #double region on a shank...
+sbatch ./MUATimescale_bla.bash  /gpfs/data/buzsakilab/DL/Database/GGData/Rat10/Rat10-20140707/ .
+sbatch ./MUATimescale_bla.bash  /gpfs/data/buzsakilab/DL/Database/GGData/Rat10/Rat10-20140708/ .
+sbatch ./MUATimescale_bla.bash  /gpfs/data/buzsakilab/DL/Database/GGData/Rat11/Rat11-20150321/ .
+sbatch ./MUATimescale_bla.bash  /gpfs/data/buzsakilab/DL/Database/GGData/Rat11/Rat11-20150323/ .
+sbatch ./MUATimescale_bla.bash  /gpfs/data/buzsakilab/DL/Database/GGData/Rat11/Rat11-20150325/ .
+sbatch ./MUATimescale_bla.bash  /gpfs/data/buzsakilab/DL/Database/GGData/Rat11/Rat11-20150326/ .
+sbatch ./MUATimescale_bla.bash  /gpfs/data/buzsakilab/DL/Database/GGData/Rat11/Rat11-20150327/ .
+sbatch ./MUATimescale_bla.bash  /gpfs/data/buzsakilab/DL/Database/GGData/Rat11/Rat11-20150328/ .
+sbatch ./MUATimescale_bla.bash  /gpfs/data/buzsakilab/DL/Database/GGData/Rat11/Rat11-20150330/ .
+#sbatch ./MUATimescale_bla.bash  /gpfs/data/buzsakilab/DL/Database/GGData/Rat11/Rat11-20150331/ .
+#sbatch ./MUATimescale_bla.bash  /gpfs/data/buzsakilab/DL/Database/GGData/Rat11/Rat11-20150401/ .
+#sbatch ./MUATimescale_bla.bash  /gpfs/data/buzsakilab/DL/Database/GGData/Rat11/Rat11-20150402/ .
+#sbatch ./MUATimescale_bla.bash  /gpfs/data/buzsakilab/DL/Database/GGData/Rat11/Rat11-20150403/ .
+
+
+sbatch ./MUATimescale_pir.bash  /gpfs/data/buzsakilab/DL/Database/GGData/Rat08/Rat08-20130708/ .
+sbatch ./MUATimescale_pir.bash  /gpfs/data/buzsakilab/DL/Database/GGData/Rat08/Rat08-20130709/ .
+sbatch ./MUATimescale_pir.bash  /gpfs/data/buzsakilab/DL/Database/GGData/Rat08/Rat08-20130710/ .
+sbatch ./MUATimescale_pir.bash  /gpfs/data/buzsakilab/DL/Database/GGData/Rat08/Rat08-20130711/ .
+sbatch ./MUATimescale_pir.bash  /gpfs/data/buzsakilab/DL/Database/GGData/Rat08/Rat08-20130712/ .
+sbatch ./MUATimescale_pir.bash  /gpfs/data/buzsakilab/DL/Database/GGData/Rat08/Rat08-20130713/ .
+sbatch ./MUATimescale_pir.bash  /gpfs/data/buzsakilab/DL/Database/GGData/Rat08/Rat08-20130715/ .
+sbatch ./MUATimescale_pir.bash  /gpfs/data/buzsakilab/DL/Database/GGData/Rat08/Rat08-20130716/ .
+sbatch ./MUATimescale_pir.bash  /gpfs/data/buzsakilab/DL/Database/GGData/Rat08/Rat08-20130717/ .
+#sbatch ./MUATimescale_pir.bash  /gpfs/data/buzsakilab/DL/Database/GGData/Rat08/Rat08-20130718/ .
+sbatch ./MUATimescale_pir.bash  /gpfs/data/buzsakilab/DL/Database/GGData/Rat08/Rat08-20130719/ .
+#sbatch ./MUATimescale_pir.bash  /gpfs/data/buzsakilab/DL/Database/GGData/Rat08/Rat08-20130720/ .
+#sbatch ./MUATimescale_pir.bash  /gpfs/data/buzsakilab/DL/Database/GGData/Rat08/Rat08-20130722/ .
+sbatch ./MUATimescale_pir.bash  /gpfs/data/buzsakilab/DL/Database/GGData/Rat09/Rat09-20140324/ .
+sbatch ./MUATimescale_pir.bash  /gpfs/data/buzsakilab/DL/Database/GGData/Rat09/Rat09-20140325/ .
+sbatch ./MUATimescale_pir.bash  /gpfs/data/buzsakilab/DL/Database/GGData/Rat09/Rat09-20140326/ .
+sbatch ./MUATimescale_pir.bash  /gpfs/data/buzsakilab/DL/Database/GGData/Rat09/Rat09-20140327/ .
+sbatch ./MUATimescale_pir.bash  /gpfs/data/buzsakilab/DL/Database/GGData/Rat09/Rat09-20140328/ .
+sbatch ./MUATimescale_pir.bash  /gpfs/data/buzsakilab/DL/Database/GGData/Rat09/Rat09-20140329/ .
+#sbatch ./MUATimescale_pir.bash  /gpfs/data/buzsakilab/DL/Database/GGData/Rat09/Rat09-20140331/ .
+#sbatch ./MUATimescale_pir.bash  /gpfs/data/buzsakilab/DL/Database/GGData/Rat09/Rat09-20140401/ .
+#sbatch ./MUATimescale_pir.bash  /gpfs/data/buzsakilab/DL/Database/GGData/Rat09/Rat09-20140402/ .
+#sbatch ./MUATimescale_pir.bash  /gpfs/data/buzsakilab/DL/Database/GGData/Rat09/Rat09-20140403/ .
+#sbatch ./MUATimescale_pir.bash  /gpfs/data/buzsakilab/DL/Database/GGData/Rat09/Rat09-20140404/ .
+#sbatch ./MUATimescale_pir.bash  /gpfs/data/buzsakilab/DL/Database/GGData/Rat09/Rat09-20140405/ .
+#sbatch ./MUATimescale_pir.bash  /gpfs/data/buzsakilab/DL/Database/GGData/Rat09/Rat09-20140407/ .
+#sbatch ./MUATimescale_pir.bash  /gpfs/data/buzsakilab/DL/Database/GGData/Rat09/Rat09-20140408/ .
+#sbatch ./MUATimescale_pir.bash  /gpfs/data/buzsakilab/DL/Database/GGData/Rat09/Rat09-20140409/ .
+sbatch ./MUATimescale_pir.bash  /gpfs/data/buzsakilab/DL/Database/GGData/Rat10/Rat10-20140619/ .
+sbatch ./MUATimescale_pir.bash  /gpfs/data/buzsakilab/DL/Database/GGData/Rat10/Rat10-20140620/ .
+sbatch ./MUATimescale_pir.bash  /gpfs/data/buzsakilab/DL/Database/GGData/Rat10/Rat10-20140622/ .
+sbatch ./MUATimescale_pir.bash  /gpfs/data/buzsakilab/DL/Database/GGData/Rat10/Rat10-20140624/ .
+sbatch ./MUATimescale_pir.bash  /gpfs/data/buzsakilab/DL/Database/GGData/Rat10/Rat10-20140626/ .
+sbatch ./MUATimescale_pir.bash  /gpfs/data/buzsakilab/DL/Database/GGData/Rat10/Rat10-20140627/ .
+sbatch ./MUATimescale_pir.bash  /gpfs/data/buzsakilab/DL/Database/GGData/Rat10/Rat10-20140628/ .
+sbatch ./MUATimescale_pir.bash  /gpfs/data/buzsakilab/DL/Database/GGData/Rat10/Rat10-20140629/ .
+sbatch ./MUATimescale_pir.bash  /gpfs/data/buzsakilab/DL/Database/GGData/Rat10/Rat10-20140701/ .
+sbatch ./MUATimescale_pir.bash  /gpfs/data/buzsakilab/DL/Database/GGData/Rat10/Rat10-20140702/ .
+sbatch ./MUATimescale_pir.bash  /gpfs/data/buzsakilab/DL/Database/GGData/Rat10/Rat10-20140703/ .
+sbatch ./MUATimescale_pir.bash  /gpfs/data/buzsakilab/DL/Database/GGData/Rat10/Rat10-20140704/ .
+#sbatch ./MUATimescale_pir.bash  /gpfs/data/buzsakilab/DL/Database/GGData/Rat10/Rat10-20140705/ .  #double region on a shank...
+sbatch ./MUATimescale_pir.bash  /gpfs/data/buzsakilab/DL/Database/GGData/Rat10/Rat10-20140707/ .
+sbatch ./MUATimescale_pir.bash  /gpfs/data/buzsakilab/DL/Database/GGData/Rat10/Rat10-20140708/ .
+sbatch ./MUATimescale_pir.bash  /gpfs/data/buzsakilab/DL/Database/GGData/Rat11/Rat11-20150321/ .
+sbatch ./MUATimescale_pir.bash  /gpfs/data/buzsakilab/DL/Database/GGData/Rat11/Rat11-20150323/ .
+sbatch ./MUATimescale_pir.bash  /gpfs/data/buzsakilab/DL/Database/GGData/Rat11/Rat11-20150325/ .
+sbatch ./MUATimescale_pir.bash  /gpfs/data/buzsakilab/DL/Database/GGData/Rat11/Rat11-20150326/ .
+sbatch ./MUATimescale_pir.bash  /gpfs/data/buzsakilab/DL/Database/GGData/Rat11/Rat11-20150327/ .
+sbatch ./MUATimescale_pir.bash  /gpfs/data/buzsakilab/DL/Database/GGData/Rat11/Rat11-20150328/ .
+sbatch ./MUATimescale_pir.bash  /gpfs/data/buzsakilab/DL/Database/GGData/Rat11/Rat11-20150330/ .
+#sbatch ./MUATimescale_pir.bash  /gpfs/data/buzsakilab/DL/Database/GGData/Rat11/Rat11-20150331/ .
+#sbatch ./MUATimescale_pir.bash  /gpfs/data/buzsakilab/DL/Database/GGData/Rat11/Rat11-20150401/ .
+#sbatch ./MUATimescale_pir.bash  /gpfs/data/buzsakilab/DL/Database/GGData/Rat11/Rat11-20150402/ .
+#sbatch ./MUATimescale_pir.bash  /gpfs/data/buzsakilab/DL/Database/GGData/Rat11/Rat11-20150403/ .
+
+
+
+sbatch ./MUATimescale.bash  /gpfs/data/buzsakilab/DL/Database/YSData/YMV01/YMV01_170818
+sbatch ./MUATimescale.bash  /gpfs/data/buzsakilab/DL/Database/YSData/YMV02/YMV02_170815
+#sbatch ./MUATimescale.bash  /gpfs/data/buzsakilab/DL/Database/YSData/YMV03/YMV03_170818
+sbatch ./MUATimescale.bash  /gpfs/data/buzsakilab/DL/Database/YSData/YMV04/YMV04_170907
+#sbatch ./MUATimescale.bash  /gpfs/data/buzsakilab/DL/Database/YSData/YMV05/YMV05_170912
+sbatch ./MUATimescale.bash  /gpfs/data/buzsakilab/DL/Database/YSData/YMV06/YMV06_170913
+#sbatch ./MUATimescale.bash  /gpfs/data/buzsakilab/DL/Database/YSData/YMV07/YMV07_170914
+#sbatch ./MUATimescale.bash  /gpfs/data/buzsakilab/DL/Database/YSData/YMV08/YMV08_170922
+sbatch ./MUATimescale.bash  /gpfs/data/buzsakilab/DL/Database/YSData/YMV09/YMV09_171204
+sbatch ./MUATimescale.bash  /gpfs/data/buzsakilab/DL/Database/YSData/YMV10/YMV10_171213
+sbatch ./MUATimescale.bash  /gpfs/data/buzsakilab/DL/Database/YSData/YMV11/YMV11_171208
+sbatch ./MUATimescale.bash  /gpfs/data/buzsakilab/DL/Database/YSData/YMV12/YMV12_171211
+#sbatch ./MUATimescale.bash  /gpfs/data/buzsakilab/DL/Database/YSData/YMV13/YMV13_180127
+sbatch ./MUATimescale.bash  /gpfs/data/buzsakilab/DL/Database/YSData/YMV14/YMV14_180128
+sbatch ./MUATimescale.bash  /gpfs/data/buzsakilab/DL/Database/YSData/YMV15/YMV15_180205
+sbatch ./MUATimescale.bash  /gpfs/data/buzsakilab/DL/Database/YSData/YMV16/YMV16_180206
+sbatch ./MUATimescale.bash  /gpfs/data/buzsakilab/DL/Database/YSData/YMV17/YMV17_180207
+sbatch ./MUATimescale.bash  /gpfs/data/buzsakilab/DL/Database/YSData/YMV18/YMV18_180208
+sbatch ./MUATimescale.bash  /gpfs/data/buzsakilab/DL/Database/YSData/YMV19/YMV19_180209
+#
+sbatch ./MUATimescale.bash  /gpfs/data/buzsakilab/DL/Database/BWData/Bogey/Bogey_012615
+sbatch ./MUATimescale.bash  /gpfs/data/buzsakilab/DL/Database/BWData/BWRat17/BWRat17_121712
+sbatch ./MUATimescale.bash  /gpfs/data/buzsakilab/DL/Database/BWData/BWRat17/BWRat17_121912
+sbatch ./MUATimescale.bash  /gpfs/data/buzsakilab/DL/Database/BWData/BWRat18/BWRat18_020513
+sbatch ./MUATimescale.bash  /gpfs/data/buzsakilab/DL/Database/BWData/BWRat19/BWRat19_032413
+sbatch ./MUATimescale.bash  /gpfs/data/buzsakilab/DL/Database/BWData/BWRat19/BWRat19_032513
+sbatch ./MUATimescale.bash  /gpfs/data/buzsakilab/DL/Database/BWData/BWRat20/BWRat20_101013
+sbatch ./MUATimescale.bash  /gpfs/data/buzsakilab/DL/Database/BWData/BWRat20/BWRat20_101513
+sbatch ./MUATimescale.bash  /gpfs/data/buzsakilab/DL/Database/BWData/BWRat21/BWRat21_121113
+sbatch ./MUATimescale.bash  /gpfs/data/buzsakilab/DL/Database/BWData/BWRat21/BWRat21_121613
+sbatch ./MUATimescale.bash  /gpfs/data/buzsakilab/DL/Database/BWData/BWRat21/BWRat21_121813
+sbatch ./MUATimescale.bash  /gpfs/data/buzsakilab/DL/Database/BWData/Dino/Dino_061814
+sbatch ./MUATimescale.bash  /gpfs/data/buzsakilab/DL/Database/BWData/Dino/Dino_061914
+sbatch ./MUATimescale.bash  /gpfs/data/buzsakilab/DL/Database/BWData/Dino/Dino_062014
+sbatch ./MUATimescale.bash  /gpfs/data/buzsakilab/DL/Database/BWData/Dino/Dino_072114
+sbatch ./MUATimescale.bash  /gpfs/data/buzsakilab/DL/Database/BWData/Dino/Dino_072314
+sbatch ./MUATimescale.bash  /gpfs/data/buzsakilab/DL/Database/BWData/Dino/Dino_072414
+sbatch ./MUATimescale.bash  /gpfs/data/buzsakilab/DL/Database/BWData/JennBuzsaki22/20140526_277um
+sbatch ./MUATimescale.bash  /gpfs/data/buzsakilab/DL/Database/BWData/JennBuzsaki22/20140527_421um
+sbatch ./MUATimescale.bash  /gpfs/data/buzsakilab/DL/Database/BWData/JennBuzsaki22/20140528_565um
+sbatch ./MUATimescale.bash  /gpfs/data/buzsakilab/DL/Database/BWData/Rizzo/Rizzo_022615
+sbatch ./MUATimescale.bash  /gpfs/data/buzsakilab/DL/Database/BWData/Rizzo/Rizzo_022715
+sbatch ./MUATimescale.bash  /gpfs/data/buzsakilab/DL/Database/BWData/Splinter/Splinter_020515
+sbatch ./MUATimescale.bash  /gpfs/data/buzsakilab/DL/Database/BWData/Splinter/Splinter_020915
+sbatch ./MUATimescale.bash  /gpfs/data/buzsakilab/DL/Database/BWData/Templeton/Templeton_032415
+
+sbatch ./MUATimescale.bash  /gpfs/data/buzsakilab/DL/Database/AGData/Achilles/Achilles_11012013
+sbatch ./MUATimescale.bash  /gpfs/data/buzsakilab/DL/Database/AGData/Achilles/Achilles_10252013
+sbatch ./MUATimescale.bash  /gpfs/data/buzsakilab/DL/Database/AGData/Buddy/Buddy_06272013
+sbatch ./MUATimescale.bash  /gpfs/data/buzsakilab/DL/Database/AGData/Cicero/Cicero_09102014
+sbatch ./MUATimescale.bash  /gpfs/data/buzsakilab/DL/Database/AGData/Cicero/Cicero_09012014
+sbatch ./MUATimescale.bash  /gpfs/data/buzsakilab/DL/Database/AGData/Cicero/Cicero_09172014
+sbatch ./MUATimescale.bash  /gpfs/data/buzsakilab/DL/Database/AGData/Gatsby/Gatsby_08282013
+sbatch ./MUATimescale.bash  /gpfs/data/buzsakilab/DL/Database/AGData/Gatsby/Gatsby_08022013
+#
+#
+#
+sbatch ./MUATimescale.bash  /gpfs/data/buzsakilab/DL/Database/APData/Mouse12/Mouse12-120806
+sbatch ./MUATimescale.bash  /gpfs/data/buzsakilab/DL/Database/APData/Mouse12/Mouse12-120807
+sbatch ./MUATimescale.bash  /gpfs/data/buzsakilab/DL/Database/APData/Mouse12/Mouse12-120808
+sbatch ./MUATimescale.bash  /gpfs/data/buzsakilab/DL/Database/APData/Mouse12/Mouse12-120809
+sbatch ./MUATimescale.bash  /gpfs/data/buzsakilab/DL/Database/APData/Mouse12/Mouse12-120810
+sbatch ./MUATimescale.bash  /gpfs/data/buzsakilab/DL/Database/APData/Mouse17/Mouse17-130125
+sbatch ./MUATimescale.bash  /gpfs/data/buzsakilab/DL/Database/APData/Mouse17/Mouse17-130128
+sbatch ./MUATimescale.bash  /gpfs/data/buzsakilab/DL/Database/APData/Mouse17/Mouse17-130129
+sbatch ./MUATimescale.bash  /gpfs/data/buzsakilab/DL/Database/APData/Mouse17/Mouse17-130130
+sbatch ./MUATimescale.bash  /gpfs/data/buzsakilab/DL/Database/APData/Mouse17/Mouse17-130131
+sbatch ./MUATimescale.bash  /gpfs/data/buzsakilab/DL/Database/APData/Mouse17/Mouse17-130201
+sbatch ./MUATimescale.bash  /gpfs/data/buzsakilab/DL/Database/APData/Mouse17/Mouse17-130202
+sbatch ./MUATimescale.bash  /gpfs/data/buzsakilab/DL/Database/APData/Mouse17/Mouse17-130203
+sbatch ./MUATimescale.bash  /gpfs/data/buzsakilab/DL/Database/APData/Mouse17/Mouse17-130204
+sbatch ./MUATimescale.bash  /gpfs/data/buzsakilab/DL/Database/APData/Mouse20/Mouse20-130514
+sbatch ./MUATimescale.bash  /gpfs/data/buzsakilab/DL/Database/APData/Mouse20/Mouse20-130515
+sbatch ./MUATimescale.bash  /gpfs/data/buzsakilab/DL/Database/APData/Mouse20/Mouse20-130516
+sbatch ./MUATimescale.bash  /gpfs/data/buzsakilab/DL/Database/APData/Mouse20/Mouse20-130517
+sbatch ./MUATimescale.bash  /gpfs/data/buzsakilab/DL/Database/APData/Mouse20/Mouse20-130520
+sbatch ./MUATimescale.bash  /gpfs/data/buzsakilab/DL/Database/APData/Mouse24/Mouse24-131213
+sbatch ./MUATimescale.bash  /gpfs/data/buzsakilab/DL/Database/APData/Mouse24/Mouse24-131216
+sbatch ./MUATimescale.bash  /gpfs/data/buzsakilab/DL/Database/APData/Mouse24/Mouse24-131217
+sbatch ./MUATimescale.bash  /gpfs/data/buzsakilab/DL/Database/APData/Mouse24/Mouse24-131218
+sbatch ./MUATimescale.bash  /gpfs/data/buzsakilab/DL/Database/APData/Mouse25/Mouse25-140123
+sbatch ./MUATimescale.bash  /gpfs/data/buzsakilab/DL/Database/APData/Mouse25/Mouse25-140124
+sbatch ./MUATimescale.bash  /gpfs/data/buzsakilab/DL/Database/APData/Mouse25/Mouse25-140128
+sbatch ./MUATimescale.bash  /gpfs/data/buzsakilab/DL/Database/APData/Mouse25/Mouse25-140129
+sbatch ./MUATimescale.bash  /gpfs/data/buzsakilab/DL/Database/APData/Mouse25/Mouse25-140130
+sbatch ./MUATimescale.bash  /gpfs/data/buzsakilab/DL/Database/APData/Mouse25/Mouse25-140131
+sbatch ./MUATimescale.bash  /gpfs/data/buzsakilab/DL/Database/APData/Mouse25/Mouse25-140203
+sbatch ./MUATimescale.bash  /gpfs/data/buzsakilab/DL/Database/APData/Mouse25/Mouse25-140204
+sbatch ./MUATimescale.bash  /gpfs/data/buzsakilab/DL/Database/APData/Mouse25/Mouse25-140205
+sbatch ./MUATimescale.bash  /gpfs/data/buzsakilab/DL/Database/APData/Mouse25/Mouse25-140206
+sbatch ./MUATimescale.bash  /gpfs/data/buzsakilab/DL/Database/APData/Mouse28/Mouse28-140310
+sbatch ./MUATimescale.bash  /gpfs/data/buzsakilab/DL/Database/APData/Mouse28/Mouse28-140311
+sbatch ./MUATimescale.bash  /gpfs/data/buzsakilab/DL/Database/APData/Mouse28/Mouse28-140312
+sbatch ./MUATimescale.bash  /gpfs/data/buzsakilab/DL/Database/APData/Mouse28/Mouse28-140313
+sbatch ./MUATimescale.bash  /gpfs/data/buzsakilab/DL/Database/APData/Mouse28/Mouse28-140314
+sbatch ./MUATimescale.bash  /gpfs/data/buzsakilab/DL/Database/APData/Mouse28/Mouse28-140317
+sbatch ./MUATimescale.bash  /gpfs/data/buzsakilab/DL/Database/APData/Mouse28/Mouse28-140318
+sbatch ./MUATimescale.bash  /gpfs/data/buzsakilab/DL/Database/APData/Mouse32/Mouse32-140822
+#
+#
+#
