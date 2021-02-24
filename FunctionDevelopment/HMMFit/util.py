9a7843ee817a48f37ca1cee4685a72edfc38146c
Dan Levenstein <dan.levenstein@gmail.com>
2021-02-24 14:20:53 -0500

    buggiefix with kiloname

--- FunctionDevelopment/HMMFit/util.py
+++ FunctionDevelopment/HMMFit/util.py
@@ -6,13 +6,13 @@ import os
 
 # Return sleep states as dictionary
 def getStates(basepath):
-    
+
     # Check for file existence
     basename = os.path.basename(basepath)
     filepath = os.path.join( basepath, basename+'.SleepState.states.mat' )
     if not os.path.isfile( filepath ):
         return -1
-    
+
     states = scio.loadmat(filepath)
     # Extract state names out of the struct
     stateNames = []
@@ -25,7 +25,7 @@ def getStates(basepath):
     SleepState = {}
     for kp in range(ntup):
         SleepState[stateNames[kp]] = states['SleepState'][0][0][0][0][0][kp]
-    
+
     return SleepState
 
 # Return kilosort file, if exists
@@ -39,11 +39,11 @@ def getKilosortFolder(basepath):
 # Return spikes
 def getSpikes(basepath, loadKilo=False):
     basename = os.path.basename(basepath)
-    kilosort_file = getKilosortFolder(basepath)
-    if kilosort_file and loadKilo:
-        spikespath = os.path.join(basepath, kilosort_file, basename+'.spikes.cellinfo.mat')
-    else:
-        spikespath = os.path.join(basepath, basename+'.spikes.cellinfo.mat')
+    # kilosort_file = getKilosortFolder(basepath)
+    # if kilosort_file and loadKilo:
+    #     spikespath = os.path.join(basepath, kilosort_file, basename+'.spikes.cellinfo.mat')
+    # else:
+    spikespath = os.path.join(basepath, basename+'.spikes.cellinfo.mat')
     out = scio.loadmat(spikespath)
     return out['spikes'][0,0]
 
@@ -65,7 +65,7 @@ def getStateDepISIs(states, spk):
     # Loop over states
     for kp in range( len(dictkey) ):
         ints = states[dictkey[kp]]
-        # Loop over epochs 
+        # Loop over epochs
         isiseq = []
         spkseq = []
         for jp in range( ints.shape[0] ):
@@ -83,24 +83,24 @@ def getStateDepISIs(states, spk):
 # Convert logrates / cvs into  rate / shape (input to pomegranate)
 def toAlphaBeta(lograte, cv):
     # NOTE: this anticipates arrays
-    
+
     # Flatten rates, make sure it's numpy
     if isinstance(lograte, list):
         lograte = np.array(lograte).flatten()
     else:
         lograte = lograte.flatten()
-    
+
     # Flatten cvs, make sure it's numpy
     if isinstance(cv, list):
         cv = np.array(cv).flatten()
     else:
         cv = cv.flatten()
-    
-    # Get the shape (alpha == k)    
+
+    # Get the shape (alpha == k)
     k = np.divide( 1, cv )
     # Get the rate (beta == lambda)
     lambd = np.multiply( np.power( 10, lograte), k )
-    
+
     return lambd, k
 
 # Take rate / shape back to lograte / CV (interpretable as the rate / CV)
@@ -125,6 +125,3 @@ def toRateCV(lambd, k):
     lograte = np.log10( np.divide(lambd, k) )
 
     return lograte, cv
-
-
-        
