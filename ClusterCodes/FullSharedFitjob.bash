537d738276f4f147adc321e283ad092be1c69889
Dan Levenstein <dan.levenstein@gmail.com>
2021-04-22 22:09:07 -0400

    upload test for multigamma

--- ClusterCodes/FullSharedFitjob.bash
+++ ClusterCodes/FullSharedFitjob.bash
@@ -1,8 +1,8 @@
 #!/bin/bash
-#SBATCH -p cpu_long
+#SBATCH -p cpu_short
 #SBATCH --nodes=1
 #SBATCH --tasks-per-node=1
-#SBATCH --time=48:00:00
+#SBATCH --time=12:00:00
 #SBATCH --mem-per-cpu=48G
 echo $1
 module load matlab/R2018a
