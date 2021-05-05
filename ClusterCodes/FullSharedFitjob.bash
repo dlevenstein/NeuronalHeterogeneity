cce15e9d28621fbb994f58ae61c2eea4c71a566a
Dan Levenstein <dan.levenstein@gmail.com>
2021-05-04 21:51:15 -0400

    full fit all regions!

--- ClusterCodes/FullSharedFitjob.bash
+++ ClusterCodes/FullSharedFitjob.bash
@@ -5,12 +5,14 @@
 #SBATCH --time=15-00:00:00
 #SBATCH --mem-per-cpu=6G
 echo $1
+echo $2
+echo $3
 module load matlab/R2018a
 
 export SCRATCH=/gpfs/scratch/dl2820
 mkdir -p $SCRATCH/$SLURM_JOB_ID
 
 
-matlab -nodisplay -nodesktop -singleCompThread -r "cd('/gpfs/data/buzsakilab/DL/NeuronalHeterogeneity/ClusterCodes');addPaths;MultiRecordingGammaModes('/gpfs/data/buzsakilab/DL/NeuronalHeterogeneity/AnalysisScripts/AnalysisFigs/SharedGammaModeFitAnalysis','saveName','CA1','clusterpar',true);exit;"
+matlab -nodisplay -nodesktop -singleCompThread -r "cd('/gpfs/data/buzsakilab/DL/NeuronalHeterogeneity/ClusterCodes');addPaths;MultiRecordingGammaModes($2,'saveName',$1,'region',$3,'saveFolder','/gpfs/data/buzsakilab/DL/NeuronalHeterogeneity/AnalysisScripts/AnalysisFigs/SharedGammaModeFitAnalysis','clusterpar',true);exit;"
 
 rm -rf $SCRATCH/$SLURM_JOB_ID
